199772d9bf6ea1e91be233a626a60951831d1956
Tereza Štětinová <schimperka@seznam.cz>
2025-01-20 19:54:44 +0100

    druhe huraa

--- 2015/02/extra_paper.py
+++ 2015/02/extra_paper.py
@@ -1,10 +1,1 @@
-# Načtení dat ze souboru 'extra_paper_input.txt'
 
-with open("extra_paper_input.txt", "r") as file:
-    raw_data = file.read().strip()  # Načte obsah a odstraní prázdné řádky na začátku a konci
-
-# Převod dat na seznam seznamů
-input_data = [list(map(int, line.split('x'))) for line in raw_data.split('\n')]
-
-# Výstup
-print(input_data)
